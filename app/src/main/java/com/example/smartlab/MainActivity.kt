95b59322304652fa8b994fed204b5139222f611f
Kraev Dmitrii <Kraevdmitrii.zamt@gmail.com>
2024-12-17 07:31:50 +0300

    added premain & main screen

--- app/src/main/java/com/example/smartlab/MainActivity.kt
+++ app/src/main/java/com/example/smartlab/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.smartlab
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.TopAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
@@ -31,8 +39,14 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.smartlab.ui.theme.HeaderColor
 import com.example.smartlab.ui.theme.SmartLabTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.HorizontalPagerIndicator
+import com.google.accompanist.pager.rememberPagerState
 import kotlinx.coroutines.delay
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,8 +66,8 @@ fun AppNavigator() {
 
     NavHost(navController, startDestination = "splash") {
         composable("splash") { SplashScreen(navController) }
-        //composable("welcome") { WelcomeScreen(navController) }
-        //composable("main") { MainScreen() }
+        composable("welcome") { WelcomeScreen(navController) }
+        composable("main") { MainScreen() }
     }
 }
 
@@ -95,7 +109,7 @@ fun SplashScreen(navController: NavController) {
             Spacer(Modifier.width(5.dp))
             Image(
                 painter = painterResource(id = R.drawable.sqlitl), // Ссылка на векторное изображение
-                contentDescription = "Лаборатория",
+                contentDescription = "..",
                 contentScale = ContentScale.Fit, // Масштабирование изображения
                 modifier = Modifier.size(30.dp) // Размер изображения
             )
@@ -103,3 +117,103 @@ fun SplashScreen(navController: NavController) {
         }
     }
 }
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun WelcomeScreen(navController: NavController) {
+    val pagerState = rememberPagerState()
+
+    val pages = listOf(
+        PageData("Анализы", "Здесь вы можете увидеть свои анализы", R.mipmap.ill_foreground, R.drawable.dots1),
+        PageData("Удобство", "Работа с врачами станет проще", R.mipmap.doc1_foreground, R.drawable.dots2),
+        PageData("Мониторинг", "Следите за состоянием здоровья", R.mipmap.doc2_foreground, R.drawable.dots3)
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Row(Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.SpaceBetween) {
+            TextButton(
+                onClick = { navController.navigate("main") },
+                modifier = Modifier
+                    //.align(Alignment.Start)
+                    .padding(16.dp)
+            ) {
+                Text("Пропустить")
+            }
+            Image(
+                painter = painterResource(id = R.drawable.sq), // Ссылка на векторное изображение
+                contentDescription = "..",
+                contentScale = ContentScale.Fit, // Масштабирование изображения
+                modifier = Modifier.size(150.dp) // Размер изображения
+                    .padding(top = 20.dp)
+            )
+
+        }
+        Row(horizontalArrangement = Arrangement.Center) {
+            HorizontalPager(
+            count = pages.size,
+            state = pagerState,
+            //modifier = Modifier.weight(1f),
+        ) { page ->
+            WelcomePage(pages[page])
+        }
+
+            HorizontalPagerIndicator(
+                pagerState = pagerState,
+                modifier = Modifier
+                    //.align(Alignment.CenterHorizontally)
+                    .padding(16.dp),
+                activeColor = Color.Blue
+            ) }
+
+    }
+}
+
+@Composable
+fun WelcomePage(page: PageData) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(page.title, fontSize = 24.sp, fontWeight = FontWeight.Bold, color = HeaderColor)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(page.description, fontSize = 16.sp, color = Color.DarkGray)
+            Image(
+                painter = painterResource(id = page.dotimgId), // Ссылка на векторное изображение
+                contentDescription = "..",
+                contentScale = ContentScale.Fit, // Масштабирование изображения
+                modifier = Modifier.size(50.dp) // Размер изображения
+                //  .padding(top = 20.dp)
+            )
+        }
+        Image(
+            painter = painterResource(id = page.imgId), // Ссылка на векторное изображение
+            contentDescription = "..",
+            contentScale = ContentScale.Fit, // Масштабирование изображения
+            modifier = Modifier.align(Alignment.BottomCenter).size(400.dp) // Размер изображения
+            //  .padding(top = 20.dp)
+        )
+    }
+}
+
+data class PageData(
+    val title: String,
+    val description: String,
+    val imgId:Int,
+    val dotimgId:Int
+)
+
+
+@Composable
+fun MainScreen() {
+    Box{}
+}
+
+
