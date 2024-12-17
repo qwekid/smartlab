0d835982befa4042027669f2290fdc94619eb5c0
Kraev Dmitrii <Kraevdmitrii.zamt@gmail.com>
2024-12-17 06:04:54 +0300

    added loader screen

--- app/src/main/java/com/example/smartlab/MainActivity.kt
+++ app/src/main/java/com/example/smartlab/MainActivity.kt
@@ -4,14 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.smartlab.ui.theme.SmartLabTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +40,66 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SmartLabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigator()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppNavigator() {
+    val navController = rememberNavController()
+
+    NavHost(navController, startDestination = "splash") {
+        composable("splash") { SplashScreen(navController) }
+        //composable("welcome") { WelcomeScreen(navController) }
+        //composable("main") { MainScreen() }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SmartLabTheme {
-        Greeting("Android")
+        AppNavigator()
+    }
+}
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    LaunchedEffect(Unit) {
+        delay(2000) // Задержка 2 секунды
+        navController.navigate("welcome") {
+            popUpTo("splash") { inclusive = true }
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(Color(0xFF4A90E2), Color(0xFF007AFF))
+                )
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Row() {
+            Text(
+                text = "Смартлаб",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Spacer(Modifier.width(5.dp))
+            Image(
+                painter = painterResource(id = R.drawable.sqlitl), // Ссылка на векторное изображение
+                contentDescription = "Лаборатория",
+                contentScale = ContentScale.Fit, // Масштабирование изображения
+                modifier = Modifier.size(30.dp) // Размер изображения
+            )
+
+        }
     }
 }
